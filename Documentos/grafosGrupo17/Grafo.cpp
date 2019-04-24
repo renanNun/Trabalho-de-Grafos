8cb50500b4e6c2b65544a1df216f2e85fe7f8f50
renanNun <renannunes400@gmail.com>
2019-04-24 10:32:23 -0300

    atualizando

--- Documentos/grafosGrupo17/Grafo.cpp
+++ Documentos/grafosGrupo17/Grafo.cpp
@@ -1,67 +1,378 @@
 #include <iostream>
-#include "Aresta.h"
+#include "Grafo.h"
 
 using namespace std;
 
 /**
-     * Construtor do TAD Aresta
-     * Constroi uma aresta e passa como parâmetro o indice do Vertice;
-     * Parâmetro: _no
+     * Construtor do TAD Grafo
+     * Constroi um Grafo (podendo ser um digrafo, ponderado ou não)
+     *
+     *
      * encapsulamento: public
      */
-Aresta::Aresta(int _no) {
-    this->noAdj = _no;
-    this->peso = -1;
-    this->prox = nullptr;
+Grafo::Grafo() {
+    this->direcional = false;
+    this->ponderadoAresta = false;
+    this->ponderadoNo = false;
+}
+
+/**
+     * Construtor do TAD Grafo
+     * Constroi um Grafo (podendo ser um digrafo, ponderado ou não)
+     * Parâmetro: entrada
+     * Parâmetro: saida
+     * encapsulamento: public
+     */
+Grafo::Grafo(string entrada, string saida) {
+    this->direcional = false;
+    this->ponderadoAresta = false;
+    this->ponderadoNo = false;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+
+/**
+     * Construtor do TAD Grafo
+     * Constroi um Grafo (podendo ser um digrafo, ponderado ou não)
+     * Parâmetro: entrada
+     * Parâmetro: saida
+     * Parâmetro: direcional
+     * encapsulamento: public
+     */
+Grafo::Grafo(string entrada, string saida, bool direcional) {
+    this->direcional = direcional;
+    this->ponderadoNo = false;
+    this->ponderadoAresta = false;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
 }
 
 
 /**
-     * Construtor do TAD Aresta
-     * Constroi uma aresta e passa como parâmetro o indice do Vertice;
-     * Parâmetro: _no
-     * Parâmetro: _peso
+     * Construtor do TAD Grafo
+     * Constroi um Grafo (podendo ser um digrafo, ponderado ou não)
+     * Parâmetro: entrada
+     * Parâmetro: saida
+     * Parâmetro: direcional
+     * Parâmetro: ponderadoAresta
      * encapsulamento: public
      */
-Aresta::Aresta(int _no, float _peso) {
-    this->noAdj = _no;
-    this->peso = _peso;
-    this->prox = nullptr;
+Grafo::Grafo(string entrada, string saida, bool direcional, bool ponderadoAresta) {
+    this->direcional = direcional;
+    this->ponderadoNo = false;
+    this->ponderadoAresta = ponderadoAresta;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+/**
+     * Construtor do TAD Grafo
+     * Constroi um Grafo (podendo ser um digrafo, ponderado ou não)
+     * Parâmetro: entrada
+     * Parâmetro: saida
+     * Parâmetro: direcional
+     * Parâmetro: ponderadoAresta
+     * Parâmetro: ponderadoNo
+     * encapsulamento: public
+     */
+Grafo::Grafo(string entrada, string saida, bool direcional, bool ponderadoNo, bool ponderadoAresta) {
+    this->direcional = direcional;
+    this->ponderadoNo = ponderadoNo;
+    this->ponderadoAresta = ponderadoAresta;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
 }
 
 /**
-     * Destrutor do TAD Aresta
-     * Deletando todas as Arestas
+     * Destrutor do TAD Grafo
      * encapsulamento: public
      */
-Aresta::~Aresta() {
-    Aresta* ant = this->prox;
-    while(a != nullptr){
-        Aresta *a = ant->getProx();
-        delete ant;
-        a = a->getProx();
+Grafo::~Grafo() {
+    delete this->listaNos;
+}
+
+
+void Grafo::imprime() {
+    cout << "DADOS: " << endl;
+    cout << "Grau: " << this->grau << endl;
+    cout << "Ordem: " << this->ordem << endl;
+    cout << "M: " << this->m << endl;
+
+    cout << "LISTA DE ADJACENCIA: " << endl;
+    for (No* n = this->listaNos; n != nullptr; n = n->getProx()) {
+
+        cout << n->getId() << " ---> ";
+
+        for (Aresta *a = n->getAresta(); a != nullptr; a = a->getProx()) {
+            // Se for ponderado nas arestas é imprimido o peso
+            if (ponderadoAresta) {
+                if (a->getProx() != nullptr)
+                    cout << a->getNoAdj() << " (" << a->getPeso() << ") ---> ";
+                else
+                    cout << a->getNoAdj() << " (" << a->getPeso() << ")";
+            } else {
+                if (a->getProx() != nullptr)
+                    cout << a->getNoAdj() << " ---> ";
+                else
+                    cout << a->getNoAdj();
+            }
+        }
+
+        cout << endl;
+
     }
 }
 
-/* Setters */
+/**
+     * Este método imprime na tela grafo no formato de lista de adjacência:
+     * Parâmetro: arquivo
+     *
+     * @param arquivo: Caminho do arquivo onde será gravada a impressão
+     */
+void Grafo::imprime(string arquivo) {
 
-void Aresta::setProx(Aresta *aresta){
-    this->prox = aresta;
+    ofstream myFile;
+
+    myFile.open(arquivo, ios::out);
+
+    if (myFile.is_open()) {
+
+        myFile << "DADOS: " << endl;
+        myFile << "Grau: " << this->grau << endl;
+        myFile << "Ordem: " << this->ordem << endl;
+        myFile << "M: " << this->m << endl;
+
+        myFile << "LISTA DE ADJACENCIA: " << endl;
+        for (No *n = this->listaNos; n != nullptr; n = n->getProx()) {
+
+            myFile << n->getId() << " ---> ";
+
+            for (Aresta *a = n->getAresta(); a != nullptr; a = a->getProx()) {
+                // Se for ponderado nas arestas é imprimido o peso
+                if (ponderadoAresta) {
+                    if (a->getProx() != nullptr)
+                        myFile << a->getNoAdj() << " (" << a->getPeso() << ") ---> ";
+                    else
+                        myFile << a->getNoAdj() << " (" << a->getPeso() << ")";
+                } else {
+                    if (a->getProx() != nullptr)
+                        myFile << a->getNoAdj() << " ---> ";
+                    else
+                        myFile << a->getNoAdj();
+                }
+            }
+
+            myFile << endl;
+
+        }
+    } else {
+        cout << "Output file does not open." << endl;
+    }
 }
 
-/* Getters */
+// *** GETTERS E SETTERS ***
 
-float Aresta::getPeso()
-{
-    return this->peso;
+
+void Grafo::setNo(int id, float peso) {
+
+    No* no = new No(id, peso);
+
+    No *list = this->listaNos;
+
+    if (list == nullptr) {
+        this->listaNos = no;
+        this->ordem = 1; // Inicializa a ordem
+        return;
+    }
+
+    // Percorrendo lista de nós até encontrar o ultimo
+    for (list; list->getProx() != nullptr; list = list->getProx()) {
+        if (list->getId() == no->getId()) {
+            //cout << "No com mesmo id ja inserido" << endl;
+            return;
+        }
+    }
+
+    // Certificando se o nó já não foi inserido
+    if (list->getId() != no->getId()) {
+        list->setProx(no);
+        this->ordem++; // Aumentando a ordem
+    }
+
 }
 
-Aresta* Aresta::getProx()
-{
-    return this->prox;
+void Grafo::setNo(int id) {
+    this->setNo(id, -1);
 }
 
-int Aresta::getNoAdj()
-{
-    return this->noAdj;
+void Grafo::setAresta(int idOrigem, int idFim, float peso) {
+    No * list = this->listaNos;
+    No * origem = nullptr;
+    No * fim = nullptr;
+
+    // Evitando self loops
+    if (idOrigem == idFim) {
+        cout << "Nao e permitido self-loop" << endl;
+        return;
+    }
+
+    // Percorrendo lista de nós até encontrar os nós que irão receber a aresta
+    for (list; list != nullptr; list = list->getProx()) {
+
+        if (list->getId() == idOrigem)
+            origem = list;
+
+        if (list->getId() == idFim)
+            fim = list;
+
+        // Evitando percorrer até o final desnecessáriamente
+        if (origem != nullptr && fim != nullptr) break;
+
+    }
+
+    if (origem != nullptr && fim != nullptr) {
+        Aresta *origem_fim = new Aresta(idFim, peso);
+
+        if (origem->setAresta(origem_fim))
+            fim->aumentaGrauEntrada();
+
+    } else {
+        cout << "No nao encontrado" << endl;
+        return;
+    }
+
+    // Se o grafo não for direcional o nó de chegada também recebe uma aresta
+    if (!this->direcional) {
+        Aresta *fim_origem = new Aresta(idOrigem, peso);
+
+        if (fim->setAresta(fim_origem))
+            origem->aumentaGrauEntrada();
+
+        // Atualizando o grau do grafo
+
+        // Como ambos graus são atualizados para grafos não direcionais
+        // Tanto faz verifivar o grau de saída ou entrada;
+        int grauOrigem = origem->getGrauEntrada();
+        int grauFim = fim->getGrauEntrada();
+
+        if (grauOrigem > grauFim)
+            this->atualizaGrau(grauOrigem);
+        else
+            this->atualizaGrau(grauFim);
+
+    }
+
+    // Atualizando grau do grafo
+
+    int grauOrigem = origem->getGrauSaida();
+    int grauFim = fim->getGrauEntrada();
+
+    if (grauOrigem > grauFim)
+        this->atualizaGrau(grauOrigem);
+    else
+        this->atualizaGrau(grauFim);
+
+    this->m++;
+
+}
+
+void Grafo::setAresta(int idOrigem, int idFim) {
+    // Verifica se o grafo é ponderado ou não da aresta para colocar um valour default
+    if (!this->ponderadoAresta)
+        this->setAresta(idOrigem, idFim, -1);
+    else
+        this->setAresta(idOrigem, idFim, 1);
+
+}
+
+
+No* Grafo::getNo(int id) {
+    No *n;
+
+    // Percorrendo lista de nós até encontrar o desejado
+    for (n = this->listaNos; n != nullptr && n->getId() != id; n = n->getProx());
+
+    return n;
+}
+
+Aresta* Grafo::getAresta(int idOrigem, int idFim) {
+    No *n = this->getNo(idOrigem); // Encontrando nó de origem
+
+    // Se encontrar nó inicia a busca se não retorna nullptr
+    if (n != nullptr) {
+        Aresta* a = n->getAresta();
+
+        // Procurando aresta com nó passado, se encontrar o retorna, se não retorna nullptr
+        for (a; a != nullptr && a->getNoAdj() != idFim; a = a->getProx());
+
+        return a;
+
+    } else {
+        return nullptr;
+    }
+
+}
+
+void Grafo::atualizaGrau(int grau) {
+    if (grau > this->grau)
+        this->grau = grau;
+}
+
+void Grafo::leitura_arquivo(string arquivo) {
+
+    ifstream file; // Leitura de arquivo
+    int n_nos = 0; // Numero de nós
+    string line; // Auxilar para leitura de cada linha
+
+    file.open(arquivo, ios::in); // Lendo arquivo de entrada
+
+    if (file.is_open()) {
+
+        getline(file, line); // Lendo a primeira linha com número de nós
+
+        n_nos = stoi(line); // Convertendo de string para int
+
+        //this->ordem = n_nos;
+
+        // Percorrendo cada linha do arquivo e adicionando a variavel dados
+        while (getline(file, line)) {
+
+            istringstream str_stream(line);
+
+            int origem, fim;
+            float peso;
+
+            // Verifica se é ponderado nas arestas
+            if (this->ponderadoAresta) {
+
+                // Lendo valores
+                str_stream >> origem >> fim >> peso;
+
+                // TODO implementar pensando que pode ser ponderado no nó
+
+                // Inserindo nós da aresta, a verificaçao de existencia é feita pelo método
+                this->setNo(origem);
+                this->setNo(fim);
+                this->setAresta(origem,fim,peso);
+
+            } else {
+
+                // Lendo valores
+                str_stream >> origem >> fim;
+
+                // TODO implementar pensando que pode ser ponderado no nó
+
+                // Inserindo nós da aresta, a verificaçao de existencia é feita pelo método
+                this->setNo(origem);
+                this->setNo(fim);
+                this->setAresta(origem,fim);
+
+            }
+
+        }
+
+        file.close();
+
+    } else {
+        cout << "Input file does not open." << endl;
+    }
 }
--- Documentos/grafosGrupo17/No.cpp
+++ Documentos/grafosGrupo17/No.cpp
@@ -1,67 +1,139 @@
-#include <iostream>
-#include "Aresta.h"
-
-using namespace std;
+##include <iostream>
+#include "No.h"
 
 /**
-     * Construtor do TAD Aresta
-     * Constroi uma aresta e passa como parâmetro o indice do Vertice;
-     * Parâmetro: _no
-     * encapsulamento: public
-     */
-Aresta::Aresta(int _no) {
-    this->noAdj = _no;
-    this->peso = -1;
-    this->prox = nullptr;
-}
-
-
-/**
-     * Construtor do TAD Aresta
-     * Constroi uma aresta e passa como parâmetro o indice do Vertice;
-     * Parâmetro: _no
+     * Construtor do TAD No
+     * Constroi um Vertice
+     * Parâmetro: _id
      * Parâmetro: _peso
      * encapsulamento: public
      */
-Aresta::Aresta(int _no, float _peso) {
-    this->noAdj = _no;
+No::No(int _id, float _peso) {
+    this->id = _id;
     this->peso = _peso;
+    this->grauSaida = 0;
+    this->grauEntrada = 0;
     this->prox = nullptr;
+    this->adj = nullptr;
 }
 
 /**
-     * Destrutor do TAD Aresta
-     * Deletando todas as Arestas
+     * Construtor do TAD No
+     * Constroi um Vertice
+     * Parâmetro: _id
      * encapsulamento: public
      */
-Aresta::~Aresta() {
-    Aresta* ant = this->prox;
-    while(a != nullptr){
-        Aresta *a = ant->getProx();
-        delete ant;
-        a = a->getProx();
+No::No(int _id) {
+    this->id = _id;
+    this->peso = -1;
+    this->grauSaida = 0;
+    this->grauEntrada = 0;
+    this->prox = nullptr;
+    this->adj = nullptr;
+}
+
+/**
+     * Destrutor do TAD No
+     * Deleta todos os Nos e todas as Arestas
+     *
+     * encapsulamento: public
+     */
+No::~No() {
+
+    // Deletando todos os nós
+    No* p = this->prox;
+    while (p != NULL){
+    No *aux;
+    aux = p->getProx();
+
+    delete p;
+
+    p = aux;
     }
+    delete this->adj;
 }
 
-/* Setters */
+/*Getters and Setters*/
 
-void Aresta::setProx(Aresta *aresta){
-    this->prox = aresta;
-}
-
-/* Getters */
-
-float Aresta::getPeso()
+int No::getId()
 {
-    return this->peso;
+  return this->id;
 }
 
-Aresta* Aresta::getProx()
+No* No::getProx()
 {
-    return this->prox;
+  return this->prox;
 }
 
-int Aresta::getNoAdj()
+float No::getPeso()
 {
-    return this->noAdj;
+  return this->peso;
+}
+
+int No::getGrauEntrada()
+{
+  return this->grauEntrada;
+}
+
+int No::getGrauSaida()
+{ return this->grauSaida;
+}
+
+Aresta *No::getAresta()
+{ return this->adj;
+}
+
+Aresta *No::getAresta(int i) {
+    Aresta *a = this->adj;
+    int c = 0;
+
+    for (a, c; a != nullptr || c < i; a = a->getProx(), c++);
+
+    if (c == i)
+        return a;
+    else
+        return nullptr;
+}
+
+void No::setProx(No *n)
+{
+  this->prox = n;
+}
+
+bool No::setAresta(Aresta *aresta) {
+    Aresta *a = this->adj;
+
+    // Inicialzando arestas
+    if (a == nullptr) {
+        this->adj = aresta;
+        this->aumentaGrauSaida();
+
+        return true;
+    }
+
+    // Encontrando a ultima aresta inserida e certificando que não existe essa aresta
+    for (a; a->getProx() != nullptr && a->getNoAdj() != aresta->getNoAdj(); a = a->getProx());
+
+    // Inserindo aresta ou imprimindo erro se já existe aresta
+    if (a->getNoAdj() != aresta->getNoAdj()) {
+        a->setProx(aresta);
+    } else {
+        std::cout << "Aresta ja inserida: " << this->getId() << " ---> " << aresta->getNoAdj() << std::endl;
+        return false;
+    }
+
+    this->aumentaGrauSaida();
+
+    return true;
+
+}
+
+void No::aumentaGrauEntrada()
+{
+  this->grauEntrada++;
+}
+
+void No::aumentaGrauSaida()
+{
+  this->grauSaida++;
 }
