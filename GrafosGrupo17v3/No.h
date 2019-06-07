2d6b2bea42ec710f889dcdabfa9028ee48205e58
renanNun <42500047+renanNun@users.noreply.github.com>
2019-06-07 13:18:35 -0300

    Comentando o código
    Comentando o No.h e exemplificando seus respectivos métodos.

--- GrafosGrupo17v3/No.h
+++ GrafosGrupo17v3/No.h
@@ -9,31 +9,31 @@ private:
     int id;
     Aresta *primeiro; //Primeiro elemento de uma lista de arestas
     No* prox; //Ponteiro para o próximo No
-    int grau;
-    float peso;
+    int grau; //Grau de um Nó
+    float peso; //Peso que um Nó carrega
 
 public:
     No();
     ~No();
 
-    int getId();
-    void setId(int id);
+    int getId(); //Retorna o ID de um Nó
+    void setId(int id); // Insere um ID para um Nó
 
-    float getPeso();
-    void setPeso(float peso);
+    float getPeso(); //Retorna o Peso de um Nó
+    void setPeso(float peso); //Insere o valor de Peso a um nó
 
-    No* getProx();
-    void setProx(No* p);
+    No* getProx(); //Retorna o ponteiro para o Próximo Nó
+    void setProx(No* p); //Define o ponteiro para o Próximo Nó
 
-    int getGrau();
-    void setGrau(int p);
-    void aumentaGrau();
-    void diminuiGrau();
+    int getGrau(); //Retorna o Grau de um Nó
+    void setGrau(int p); //Define o Grau de um Nó
+    void aumentaGrau(); //Aumenta o Grau de um Nó
+    void diminuiGrau(); //Diminui o Grau de um Nó
 
-    Aresta* getAresta();
-    void setAresta(Aresta* a);
-    void insereAresta(int no, Aresta* a);
-    void removeAresta(int no2);
+    Aresta* getAresta(); //Retorna o primeiro elemento de uma lista de Arestas daquele Nó
+    void setAresta(Aresta* a); //Define o primeiro elemento de uma lista de Arestas daquele Nó
+    void insereAresta(int no, Aresta* a); //Insere uma aresta na lista de Nós
+    void removeAresta(int no2); //Remove uma aresta da lista de Nós
 
 
 };
