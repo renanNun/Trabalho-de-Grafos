f841177936b954b02e4dd540a4c548280072bca1
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2019-06-07 11:44:13 -0300

    Metodo Insere nó e getLength concluidos

--- GrafosGrupo17v3/Utils/ListaDeNos.cpp
+++ GrafosGrupo17v3/Utils/ListaDeNos.cpp
@@ -15,16 +15,46 @@ ListaDeNos::~ListaDeNos()
     //dtor
 }
 
+int ListaDeNos::getLength(){
+    return this->length;
+}
+
 void ListaDeNos::adicionaNo(No* novoNo){
     if(this->ultimo != nullptr){
         ItemListaDeNos* novoItem = new ItemListaDeNos(ultimo, nullptr, novoNo);
         ultimo->setProximo(novoItem);
         this->ultimo = novoItem;
+        length++;
     }
     else{
         ItemListaDeNos* novoItem = new ItemListaDeNos(nullptr, nullptr, novoNo);
         this->primeiro = novoItem;
         this->ultimo = novoItem;
+        length++;
+    }
+}
+
+void ListaDeNos::insereNo(int posicao, No* novoNo){
+    if (posicao >= length){
+        this->adicionaNo(novoNo);
+        std::cout<< "Posicao maior ou igual ao tamanho da lista, o no sera adicionado na ultima posicao" << std::endl;
+    }
+    else if (posicao == 0){
+        ItemListaDeNos* novoItem = new ItemListaDeNos(nullptr, this->primeiro ,novoNo);
+        this->primeiro->setAnterior(novoItem);
+        this->primeiro = novoItem;
+        length++;
+    }
+    else{
+        int i;
+        ItemListaDeNos* antigoItemDaPosicao = this->primeiro;
+        for(i = 0; i<posicao;i++){
+            antigoItemDaPosicao = antigoItemDaPosicao->getProximo();
+            i++;
+        }
+        ItemListaDeNos* novoItem = new ItemListaDeNos(antigoItemDaPosicao->getAnterior(), antigoItemDaPosicao, novoNo);
+        antigoItemDaPosicao->getAnterior()->setProximo(novoItem);
+        antigoItemDaPosicao->setAnterior(novoItem);
     }
 }
 
--- GrafosGrupo17v3/Utils/ListaDeNos.h
+++ GrafosGrupo17v3/Utils/ListaDeNos.h
@@ -14,6 +14,7 @@ private:
 public:
     ListaDeNos();
     ~ListaDeNos();
+    int getLength();
     void adicionaNo(No* novoNo);
     void insereNo(int posicao, No* novoNo);
     ItemListaDeNos* getItem(int posicao);
