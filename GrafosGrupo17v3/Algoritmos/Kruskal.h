832674b5b3c9133b7b68c842eeba887d17fa74f8
renanNun <42500047+renanNun@users.noreply.github.com>
2019-06-13 18:28:39 -0300

    Init Kruskal.h
    Eu não sei escrever o nome desses algoritmos, perdoem eventuais erros.

---
+++ GrafosGrupo17v3/Algoritmos/Kruskal.h
@@ -0,0 +1,16 @@
+#ifndef KRUSKAL_H_INCLUDED
+#define KRUSKAL_H_INCLUDED
+
+#include "../Grafo.h"
+
+class Kruskal{
+
+private:
+
+public:
+    Kruskal(Grafo &g);
+    ~Kruskal();
+
+};
+
+#endif // KRUSKAL_H_INCLUDED
