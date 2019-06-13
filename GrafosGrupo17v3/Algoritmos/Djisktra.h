156188f4beb3f25dcad6cf6d4bef1b33236bace8
renanNun <42500047+renanNun@users.noreply.github.com>
2019-06-13 18:19:38 -0300

    Update Djisktra.h
    Algoritmo de Djisktra implementado

--- GrafosGrupo17v3/Algoritmos/Djisktra.h
+++ GrafosGrupo17v3/Algoritmos/Djisktra.h
@@ -2,17 +2,19 @@
 #define DJISKTRA_H_INCLUDED
 
 #include "../Grafo.h"
+#include <limits.h>
+
 
 class Djisktra{
 
 private:
-    void algoritmoDjisktra(Grafo &g, int noIncial, int estimativaMax)
-    {
-        int* vertices = new int[g.getNumNos()]; //vertice é a mesma coisa que No
-        int* distancias = new int[g.getNumNos()];
-        int* predecessores = new int[g.getNumNos()];
-        int* abertos = new int[g.getNumNos()];
+     int* vertices = new int[g.getNumNos()]; //vertice é a mesma coisa que No
+     int* distancias = new int[g.getNumNos()];
+     int* predecessores = new int[g.getNumNos()];
+     int* abertos = new int[g.getNumNos()];
 
+    void algoritmoDjisktra(Grafo &g, int noIncial)
+    {
         No* p = g.getPrimeiroNo();
 
         while(p != NULL){
@@ -25,7 +27,7 @@ private:
                 distancias[i] = 0;
                 predecessores[i] = p->getId();
             } else {
-                distancias[i] = estimativaMax;
+                distancias[i] = INT_MAX;
                 predecessores[i] = -1;
             }
 
@@ -40,19 +42,73 @@ private:
 
         while(numNosAbertos > 0){
 
-            indiceComMenorEstimativa = //auxiliar que calcula isso aqui.
+            indiceComMenorEstimativa = buscaIndiceComMenorEstimativa(distancias,abertos,g.getNumNos());
 
+            if(abertos[indiceComMenorEstimativa] == -1){
+
+                abertos[indiceComMenorEstimativa] = 1;
+
+                adj = g.buscaNo(vertices[indiceComMenorEstimativa])->getAresta();
+
+                while(adj != NULL){
+                    if(distancias[indiceComMenorEstimativa] + adj->getPeso() < distancias[g.buscaIndice(adj->getId())]){
+                        distancias[g.buscaIndice(adj->getId())] = distancias[indiceComMenorEstimativa] + adj->getPeso();
+                        predecessores[g.buscaIndice(adj->getId())] = vertices[indiceComMenorEstimativa];
+                    }
+                    adj = adj->getProx();
+                }
+            }
+            numNosAbertos--;
+        }
+
+        std::cout<<"DISTANCIAS MINIMAS DO VERTICE %d PARA O RESTANTE DOS VERTICES DO GRAFO :\n", noInicial << std::endl;
+
+        for(int i = 0; i < g.getNumNos();i++){
+            std::cout<<"PARA O VERTICE %d = %d\n",  vertice[i],distancias[i] << std::endl;
         }
 
     };
 
+    int buscaIndiceComMenorEstimativa(int* distancias, int* abertos, int tam)
+    {
+
+      int menor;
+      int indice = 0;
+
+      for(int i = 0; i < tam; i++){
+        if (abertos[i] == -1)
+            menor = distancias[i];
+      }
+
+      for(int i = 0; i < tam; i++){
+        if(abertos[i] == -1){
+            if(distancias[i] < menor){
+                menor = distancias[i];
+                indice = i;
+            }
+        }
+      }
+
+        return indice;
+    };
+
 public:
-    Djisktra(){
-        //Construtor
+    Djisktra(Grafo& g, int noInicial){
+        vertices = new int[g.getNumNos()]; //vertice é a mesma coisa que No
+        distancias = new int[g.getNumNos()];
+        predecessores = new int[g.getNumNos()];
+        abertos = new int[g.getNumNos()];
+
+        algoritmoDjisktra(g,noInicial);
     };
 
     ~Djisktra(){
-        //Destrutor
+
+        delete [] vertices;
+        delete [] distancias;
+        delete [] predecessores;
+        delete [] abertos;
+
     };
 };
 
