5b8f22ba9ae5968eb2c680572fb4299b9e2fbbc0
renanNun <42500047+renanNun@users.noreply.github.com>
2019-06-15 17:50:49 -0300

    Update Floyd.h
    Comentando as funcionalidades do Algoritmo e iniciando laço triplo. Isso tem O(3n) ou O(n^3)?

--- GrafosGrupo17v3/Algoritmos/Floyd.h
+++ GrafosGrupo17v3/Algoritmos/Floyd.h
@@ -3,6 +3,8 @@
 
 #include "../Grafo.h"
 
+/*** O Problema disso aqui é que tem um custo computacional do caralho, 3 laços ***/
+
 class Floyd{
 
 private:
@@ -17,10 +19,27 @@ private:
 
 public:
     Floyd(Grafo& g){
-        this->n = g.getNumNos();
-        A = new int*[n+1];
-        B = new int*[n+1];
+        this->n = g.getNumNos(); // N é o tamanho do grafo/ Tamanho do Grafo
+        A = new int*[n+1]; //Cria uma lista com Tamanho n+1
+        B = new int*[n+1]; //Cria uma lista com Tamanho n+1
 
+        for(int i = 0; i < g.getNumNos(); i++){
+
+            A[i] = new int*[n+1]; //Cria uma lista de listas/ Uma matriz
+            B[i] = new int*[n+1]; //Cria uma lista de listas/ Uma matriz
+
+        }
+
+        /*** Três Laços para a resolução da matriz de distâncias de Floyd ***/
+        for(int k = 1; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(/*Distância[i][j]*/> /*distancia[i][k] + distancia[k][j]*/){
+                        /*Distância[i][j] = distância[i][k] + distância[k][j]*/
+                    }
+                }
+            }
+        }
 
     };
     ~Floyd(){};
